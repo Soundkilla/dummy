5a5056c71718b4f2c5f8c5c1dd3860b25b642f13
imbelyan <687>
2017-06-30 15:30:50 +0300

    конфиг. пушить в ветку testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,17 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.logging.*;
 
 public class Controller {
+
+
+
     @FXML
     public Label resultlabel;
     public TextField resultlabelA;
@@ -17,16 +23,32 @@ public class Controller {
     @FXML
     protected void SubmitButton(ActionEvent event) {
         try {
+            FileInputStream fis;
+            Properties property = new Properties();
+
+            try {
+                fis = new FileInputStream("config.ini");
+                property.load(fis);
+
+
+            } catch (IOException e) {
+                System.err.println("Конфиг не найден");
+            }
+
             FileHandler fh = new FileHandler("Main.txt");
             logger.addHandler(fh);
             Double a = Double.parseDouble(resultlabelA.getText());
             Double b = Double.parseDouble(resultlabelB.getText());
             Double c = Double.parseDouble(resultlabelC.getText());
+            boolean ugol = Boolean.valueOf(property.getProperty("pryamoyugol"));
 
-            if (a + b > c && a + c > b && b + c > a) {
+            if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0) {
                 if (((Math.pow(a, 2) + Math.pow(b, 2)) < Math.pow(c, 2)) || (((Math.pow(a, 2) + Math.pow(b, 2)) < Math.pow(a, 2))) || ((Math.pow(a, 2) + Math.pow(c, 2)) < Math.pow(b, 2)) || ((Math.pow(a, 2) + Math.pow(c, 2)) < Math.pow(b, 2))) {
+                    resultlabel.setText("Тупоугольный");}
+                    else if (ugol && ((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2)) || (((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(a, 2))) || ((Math.pow(a, 2) + Math.pow(c, 2)) == Math.pow(b, 2)) || ((Math.pow(a, 2) + Math.pow(c, 2)) == Math.pow(b, 2))){
                     resultlabel.setText("Тупоугольный");
-                } else {
+                }
+                else {
                     resultlabel.setText("Не тупоугольный");
                 }
             } else {
